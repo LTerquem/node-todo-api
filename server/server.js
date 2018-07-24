ee59dcd5b2b8f6c6dea45808582a11956c1b742a
MetaAnr <metaxxxxx@gmail.com>
2018-07-24 10:40:51 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const {mongoose} = require("./db/mongoose");
-const {Todo} = require("./models/Todo");
+const {Todo} = require("./models/todo");
 const {User} = require("./models/User");
 
 var app = express();
@@ -21,3 +21,5 @@ app.post("/todos", (req, res) => {
 app.listen(3000, () => {
 	console.log("Started on port 3000");
 })
+
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,49 @@
+const expect = require("expect");
+const request = require("supertest");
+
+const {app} = require("./../server");
+const {Todo} = require("./../models/todo");
+
+beforeEach( done => {
+	Todo.remove({}).then(() => done());
+})
+
+describe("POST /todo", () => {
+	it("should create a new todo", done => {
+		var text = "Test ToDo creation";
+		request(app)
+			.post("/todos")
+			.send({text})
+			.expect(200)
+			.expect( res => expect(res.body.text).toBe(text))
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+
+				Todo.find()
+					.then(todos => {
+						expect(todos.length).toBe(1);
+						expect(todos[0].text).toBe(text);
+						done();
+					}).catch( err => done(err));
+			});
+	});
+
+	it("should not create a todo - invalid body data", done => {
+		request(app)
+			.post("/todos")
+			.send({})
+			.expect(400)
+			.end( (err, res) => {
+				if(err) {
+					return done(err);
+				}
+				Todo.find()
+					.then( todos => {
+						expect(todos.length).toBe(0);
+						done();
+					}).catch( err => done(err))
+			});
+	});
+})
